32c273b97afed0c0778064a0dd9bd3aee881c23d
Fabio Calefato <fcalefato@gmail.com>
2013-03-31 10:18:16 +0200

    fixed issue 1

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class CorsaroNero(TorrentMagnetProvider):
 	### TODO: are animated movies released to DVDrip or Anime category? ###
 	cat_ids = [
 		(1, 'DVDrip'),
-		#(5, 'Anime'),
+		# (5, 'Anime'),
 	]
 
 	http_time_between_calls = 1  # seconds
@@ -29,73 +29,28 @@ class CorsaroNero(TorrentMagnetProvider):
 
 	### TODO: what about movie year and quality? ###
 	def _searchOnTitle(self, title, movie, quality, results):
+		log.debug("Searching for %s (%s) on %s" % (title, quality, self.urls['base_url']))
 		# remove accents 
 		simpletitle = simplifyString(title)
 		data = self.getHTMLData(self.urls['search'] % (1, tryUrlencode(simpletitle)))
-		
+
 		if 'Nessus torrent trovato!!!!' in data:
 			log.info("No torrents found for %s on ilCorsaroNero.info.", title)
 			return
 		
 		if data:
-			table_order = ['Cat', 'Name', 'Size', 'Azione', 'Data', 'S', 'L']
-
 			try:
 				html = BeautifulSoup(data)
 				resultdiv = html.find('div', attrs={'id': 'left'})
-				resulttable = resultdiv.find('tbody')
-				entries = resulttable.find_all('tr')		
-				
-				for result in entries:
-					try:
-
-						if not result.get('class') or result['class'][0] == 'bordo':
-							continue
-						
-						new = {}
-
-						nr = 0
-						for td in result.find_all('td'):
-							column_name = table_order[nr]
-							if column_name:
-								
-								if column_name is 'Cat':
-									cat = td.find('a', {'class': 'red'}).text
-									# category must be in cat_ids to go on, otherwise break inner cicle and move to next result
-									if cat == self.cat_ids[0][1]: #or cat == self.cat_ids[1][1]:
-										log.info("Hit right category: %s is a movie, keep going.", (cat))
-										td.next
-									else:
-										log.info("Wrong category: %s not a movie, skipping.", (cat))
-										break										
-								elif column_name is 'Name':
-									link = td.find('a', {'class': 'tab'})										
-									new['name'] = link.text									
-								elif column_name is 'Size':
-									new['size'] = self.parseSize(td.text)
-								elif column_name is 'Azione':
-									# retrieve download link
-									new['detail_url'] = td.find('form')['action']
-									new['id'] = new['detail_url'].split('/')[4]
-									# fare richiesta detail url e prendere link magnet
-									new['url'] = self.getMagnetLink(new['detail_url'])
-								elif column_name is 'Data':
-									new['age'] = self.ageToDays(td.find('font').text)
-								elif column_name is 'S':
-									new['seeders'] = tryInt(td.find('font').text)
-								elif column_name is 'L':
-									new['leechers'] = tryInt(td.find('font').text)
-									### TODO: what about score extras here ??? ###
-									new['score'] = 0
-
-							nr += 1
-
-						if nr == 7: # only if we parsed all tds (i.e. category was right)
-							results.append(new)
-					except:
-						log.error('Failed parsing ilCorsaroNero: %s', traceback.format_exc())
-				pass
+				entries_1 = resultdiv.find_all('tr', attrs={'class':'odd'})
+				entries_2 = resultdiv.find_all('tr', attrs={'class':'odd2'})
 			
+				try:
+					self.parseResults(results, entries_1)
+					self.parseResults(results, entries_2)
+				except:
+					log.error('Failed parsing ilCorsaroNero: %s', traceback.format_exc())
+						
 			except AttributeError:
 				log.debug('No search results found.')
 
@@ -117,3 +72,49 @@ class CorsaroNero(TorrentMagnetProvider):
 		magnet = html.find('a', attrs={'class': 'forbtn'})['href']
 		return magnet
 	
+	# filters the <td> elements containing the results, if any
+	def parseResults(self, results, entries):
+		table_order = ['Cat', 'Name', 'Size', 'Azione', 'Data', 'S', 'L']
+		
+		for result in entries:
+			new = {}
+			nr = 0
+	
+			for td in result.find_all('td'):
+				column_name = table_order[nr]
+				if column_name:
+	
+					if column_name is 'Cat':
+						cat = td.find('a', {'class': 'red'}).text
+						# category must be in cat_ids to go on, otherwise break inner cicle and move to next result
+						if cat == self.cat_ids[0][1]:  # or cat == self.cat_ids[1][1]:
+							log.info("Hit right category: %s is a movie, keep going.", (cat))
+							td.next
+						else:
+							log.info("Wrong category: %s not a movie, skipping.", (cat))
+							break										
+					elif column_name is 'Name':
+						link = td.find('a', {'class': 'tab'})										
+						new['name'] = link.text									
+					elif column_name is 'Size':
+						new['size'] = self.parseSize(td.text)
+					elif column_name is 'Azione':
+						# retrieve download link
+						new['detail_url'] = td.find('form')['action']
+						new['id'] = new['detail_url'].split('/')[4]
+						# fare richiesta detail url e prendere link magnet
+						new['url'] = self.getMagnetLink(new['detail_url'])
+					elif column_name is 'Data':
+						new['age'] = self.ageToDays(td.find('font').text)
+					elif column_name is 'S':
+						new['seeders'] = tryInt(td.find('font').text)
+					elif column_name is 'L':
+						new['leechers'] = tryInt(td.find('font').text)
+						### TODO: what about score extras here ??? ###
+						new['score'] = 0
+	
+				nr += 1
+	
+			if nr == 7:  # only if we parsed all tds (i.e. category was right)
+				results.append(new)
+				log.debug("New result %s", new)
