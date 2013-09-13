6208f173a14e167fd47774066c5c8f13847c7387
Fabio Calefato <fabio.calefato@uniba.it>
2013-09-13 15:09:54 +0200

    Added seed ratio and time options

--- __init__.py
+++ __init__.py
@@ -18,13 +18,27 @@ config = [{
                     'type': 'enabler',
                     'default': False,
                 },
-                                        {
+                {
                     'name': 'extra_score',
                     'advanced': True,
                     'label': 'Extra Score',
                     'type': 'int',
                     'default': 20,
                     'description': 'Starting score for each release found via this provider.',
+                },
+                {
+                    'name': 'seed_ratio',
+                    'label': 'Seed ratio',
+                    'type': 'float',
+                    'default': 1,
+                    'description': 'Will not be (re)moved until this seed ratio is met.',
+                },
+                {
+                    'name': 'seed_time',
+                    'label': 'Seed time',
+                    'type': 'int',
+                    'default': 40,
+                    'description': 'Will not be (re)moved until this seed time (in hours) is met.',
                 }
             ],
         },
